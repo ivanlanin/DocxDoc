b0653aacf6a53eb5294bed85bbda7f698ab26eb4
Ivan Lanin <ivanlanin@gmail.com>
2014-06-04 09:06:39 +0700

    First working sample

---
+++ samples/explorer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Explorer sample
+ */
+
+include_once __DIR__ . '/../src/DocxDoc/Explorer.php';
+
+use DocxDoc\Explorer;
+
+$filename = __DIR__ . '/resources/doc.docx';
+$explorer = new Explorer($filename);
+$files = $explorer->parsePackage();
+echo implode('<br />', $files);
---
+++ src/DocxDoc/Explorer.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * This file is part of DocxDoc, a collection of utilities to parse or
+ * manipulate OOXML DOCX. DocxDoc is a free software distributed under MIT
+ * License. For the full copyright and license information, please view the
+ * LICENSE file.
+ */
+
+namespace DocxDoc;
+
+/**
+ * Docx explorer class
+ */
+class Explorer
+{
+    /**
+     * Filename
+     *
+     * @var string
+     */
+    private $filename;
+
+    /**
+     * Create instance
+     *
+     * @param string $filename
+     */
+    public function __construct($filename)
+    {
+        $this->filename = $filename;
+    }
+
+    /**
+     * Parse package and return list of files
+     *
+     * @return array
+     */
+    public function parsePackage()
+    {
+        if (file_exists($this->filename) === false) {
+            throw new \Exception('File does not exists.');
+        }
+
+        $files = array();
+        $zip = new \ZipArchive();
+        $zip->open($this->filename);
+        for ($i = 0; $i < $zip->numFiles; $i++ ) {
+            $file = $zip->statIndex($i);
+            $path = pathinfo($file['name']);
+            if ($path['dirname'] != '.') {
+                $files[] = $file['name'];
+            }
+            sort($files);
+        }
+
+        return $files;
+    }
+}
